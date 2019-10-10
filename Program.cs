0cabb01a4ae6a7b349d52bcdb83309986d2ae376
Nivedita Purohit <npurohit@AIENTERPRISE.COM>
2019-10-10 11:17:02 +0530

    db chnages

--- Program.cs
+++ Program.cs
@@ -1,86 +1,172 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
-using System.Data.Common;
 using System.Data.OleDb;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ConsoleTestApp.Globals;
+
+
 
 namespace ConsoleTestApp
 {
     class Program
     {
-       
-        static void Main(string[] args)
+        /// <summary>
+        /// This method returns excelsheet of specified path in parameter
+        /// </summary>
+        /// <param name="path">Excel Sheet Parameter</param>
+        /// <returns>Returns excel sheet in dataTable format</returns>
+        public DataTable getExcelSheet(string path)
         {
-            string filePath;
-            Console.WriteLine("Enter the path to read Excel file");
-            filePath = Console.ReadLine();
-            string con =@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+filePath+";" +
-                        @"Extended Properties='Excel 8.0;HDR=Yes;'";
-            using (OleDbConnection connection = new OleDbConnection(con))
+            DataTable excelData = new DataTable();
+            string excelConString = null, conString;
+            excelConString = ConfigurationManager.ConnectionStrings["ExcelConStringxls"].ConnectionString;
+            conString = string.Format(excelConString, path);
+            try
             {
-                connection.Open();
-                OleDbCommand command = new OleDbCommand("select * from [Sheet1$]", connection);
-                using (OleDbDataReader dr = command.ExecuteReader())
+                using (OleDbConnection connection = new OleDbConnection(conString))
                 {
-                    var dataTabel = new DataTable();
-                    dataTabel.TableName= "";
-                    dataTabel.Load(dr);
-                   foreach(var col in dataTabel.Columns)
-                    {                        
-                        Console.Write(col);
-                        Console.Write("\t");
-                    }
-                   foreach(DataRow row in dataTabel.Rows)
-                    {
-                        Console.WriteLine();
-                        Console.Write("\t");
-                        foreach (var item in row.ItemArray)
-                        {                           
-                            Console.Write(item);
-                            Console.Write("\t");
-                        }
-                    }
-                    Console.ReadLine();
-
-
-
-                    
-             
-
-                    //Create Excel Connection
-                    string ConStr;
-                    string HDR;
-                    HDR = "YES";
-                    ConStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source="
-                        + filePath + ";Extended Properties=\"Excel 12.0;HDR=" + HDR + ";IMEX=0\"";
-                    OleDbConnection cnn = new OleDbConnection(ConStr);
-
-                    //Get data from Excel Sheet to DataTable
-                    OleDbConnection Connection = new OleDbConnection(ConStr);
-                    Connection.Open();
-                    OleDbCommand oconn = new OleDbCommand("select * from [Sheet2 $]", Connection);
-                    OleDbDataAdapter adp = new OleDbDataAdapter(oconn);
-                    DataTable dt = new DataTable();
-                    adp.Fill(dt);
-                    Connection.Close();
-
                     connection.Open();
-                    //Load Data from DataTable to SQL Server Table.
-                    using (SqlBulkCopy BC = new SqlBulkCopy(ConStr))
+                    using (OleDbCommand excelCommand = new OleDbCommand("select * from [DailyUpdate$]", connection))
                     {
-                       // BC.DestinationTableName = Tracker + "." + tblCustomer;
-                        foreach (var column in dt.Columns)
-                            BC.ColumnMappings.Add(column.ToString(), column.ToString());
-                        BC.WriteToServer(dt);
+                        using (OleDbDataReader data = excelCommand.ExecuteReader())
+                        {
+                            excelData.TableName = "DailyTracker";
+                            excelData.Load(data);
+                        }
+
                     }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+
+            return excelData;
+        }
+        /// <summary>
+        /// This method displays excel sheet on console
+        /// </summary>
+        /// <param name="dataTable"></param>
+        public void displayExcelOnConsole(DataTable dataTable)
+        {
+            foreach (var col in dataTable.Columns)
+            {
+                Console.Write(col);
+                Console.Write("\t");
+            }
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine();
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item);
+                    Console.Write("\t");
+                }
+            }
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// This method inserts excel sheet values into TaskStatus table
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns>Returns number or rows affected</returns>
+        public int insertExcelToDatabase(DataTable dataTable)
+        {
+            int rows = 0;
+            string conString = ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                try
+                {
+                    string sqlCommand = "insert into taskstatus([taskid],[taskstatus]) values (@taskid,@taskstatus)";
+                    connection.Open();
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        using (SqlCommand cmd = new SqlCommand(sqlCommand, connection))
+                        {
+                            cmd.Parameters.Add("@taskid", SqlDbType.Int).Value = dataTable.Rows[i]["TaskId"];
+                            cmd.Parameters.Add("@taskstatus", SqlDbType.NVarChar).Value = dataTable.Rows[i]["TaskStatus"];
+                            rows = cmd.ExecuteNonQuery();
+                        }
+
+                    }
+
                     connection.Close();
+                    return rows;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.ReadLine();
+                    return rows;
+
                 }
             }
         }
-        
+        /// <summary>
+        /// This method is used to execute user query
+        /// </summary>
+        /// <param name="query"></param>
+        public void executeUserQuery(string query)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
+            try
+            {
+                using (SqlConnection conneciton = new SqlConnection(conString))
+                {
+                    conneciton.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conneciton))
+                    {
+                        using (SqlDataReader data = cmd.ExecuteReader())
+                        {
+                            while (data.Read())
+                            {
+                                for (int i = 0; i < data.FieldCount; i++)
+                                {
+                                    Console.WriteLine(data.GetValue(i));
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string filePath;
+            string userQuery;
+            System.Console.WriteLine("Enter the path to read Excel file");
+            filePath = Console.ReadLine();
+
+            Program obj = new Program();//Object for class program
+
+            DataTable excelSheet = obj.getExcelSheet(filePath);//retrieves excelsheet
+
+            obj.displayExcelOnConsole(excelSheet);//displays excelsheet on console
+
+            int rowsAffectd = obj.insertExcelToDatabase(excelSheet);
+
+            System.Console.WriteLine("Enter query to execute");
+            //query to read from database is remaining
+            userQuery = Console.ReadLine();
+
+            obj.executeUserQuery(userQuery);
+
+        }
     }
 }
